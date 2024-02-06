43f57e4b7c6c33cc7e93af216dbdd41723a4d32c
Nyks Nivero <nia.illust@gmail.com>
2024-02-06 09:27:40 +0800

    added slot0 or newest option to overlay

--- v2_2/scenes/debug/overlay.gd
+++ v2_2/scenes/debug/overlay.gd
@@ -3,17 +3,26 @@ extends CanvasLayer
 
 
 func _ready():
-	$HBoxContainer/LoadButton.disabled = not SaveManager.is_slot_in_use(0)
+	refresh_load_button()
 	
 
-
 func _on_load_button_pressed():
-	Game.load_state(SaveManager.load_from_slot(0))
+	Game.load_state(SaveManager.load_from_slot(save_slot()))
 	
 
 func _on_save_button_pressed():
-	SaveManager.save_to_slot(Game.save_state(), 0)
-	$HBoxContainer/LoadButton.disabled = not SaveManager.is_slot_in_use(0)
+	SaveManager.save_to_slot(Game.save_state(), save_slot())
+	refresh_load_button()
+	
+	
+func save_slot() -> int:
+	if ($OptionButton.selected == 0) or (Persistent.newest_save_slot == -1):
+		return 0
+	return Persistent.newest_save_slot
+	
+	
+func refresh_load_button():
+	$HBoxContainer/LoadButton.disabled = not SaveManager.is_slot_in_use(save_slot())
 	
 
 func _on_quit_button_pressed():
