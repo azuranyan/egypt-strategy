317593952152f62bae8285aa6636f643d32c76e8
Nyks Nivero <nia.illust@gmail.com>
2024-02-07 18:41:05 +0800

    battle result update

--- v2_2/main/game_logic.gd
+++ v2_2/main/game_logic.gd
@@ -12,9 +12,21 @@ func _ready():
 	Game.overworld_turn_started.connect(func(empire): print('%s turn started' % empire.leader_name()))
 	Game.overworld_turn_ended.connect(func(empire): print('%s turn ended' % empire.leader_name()))
 
+	Game.battle_ended.connect(show_parsed_result)
 	Game.empire_defeated.connect(empire_defeated)
 
 
+func show_parsed_result(result: BattleResult):
+	print("result={value}; attacker={attacker}; defender={defender}; territory={territory}; player_participating={player_participating}; player_won={player_won}".format({
+		value = result.value,
+		attacker = result.attacker.leader_id,
+		defender = result.defender.leader_id,
+		territory = result.territory.name,
+		player_participating = result.is_player_participating(),
+		player_won = result.player_won(),
+	}))
+	
+	
 func empire_defeated(empire: Empire):
 	print('%s defeated' % empire.leader_name())
 	if empire.is_random():
